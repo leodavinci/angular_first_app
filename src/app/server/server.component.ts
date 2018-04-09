0eff2b7bf145f7bc242f475541c3396308315c29
Mayank Joshi <mayank@aisle.co>
2018-04-10 01:16:48 +0530

    adding first component

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-server',
+    // templateUrl: './server.component.html'
+    template: '<h4>I am server component 2!!!</h4>'
+})
+export class ServerComponent {
+
+}
