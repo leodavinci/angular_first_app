6e8654ef155a84d8acdf7787f644089e03549384
Mayank Joshi <mayank@aisle.co>
2018-04-10 01:26:53 +0530

    changes

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-server',
+    // selector: 'app-server',
+    // selector: '[app-server]',
+    selector: '.app-server',
     // templateUrl: './server.component.html'
     template: '<h4>I am server component 2!!!</h4>'
 })
